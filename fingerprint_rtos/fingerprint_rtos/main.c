cb696c50948d9900f5999ce40319b546c62e80a6
Nevena Maric <nevena.maric@avisto-eastern.com>
2021-08-03 11:39:44 +0200

    Delete unnecessary code

--- fingerprint_rtos/fingerprint_rtos/main.c
+++ fingerprint_rtos/fingerprint_rtos/main.c
@@ -55,28 +55,11 @@
 
 #define TASKSTACKSIZE   512
 
-Task_Struct task0Struct;
-Char task0Stack[TASKSTACKSIZE];
-
-/*
- *  ======== heartBeatFxn ========
- *  Toggle the Board_LED0. The Task_sleep is determined by arg0 which
- *  is configured for the heartBeat Task instance.
- */
-Void heartBeatFxn(UArg arg0, UArg arg1)
-{
-    while (1) {
-        Task_sleep((UInt)arg0);
-        GPIO_toggle(Board_LED0);
-    }
-}
-
 /*
  *  ======== main ========
  */
 int main(void)
 {
-    Task_Params taskParams;
 
     /* Call board init functions */
     Board_initGeneral();
@@ -89,20 +72,8 @@ int main(void)
     // Board_initWatchdog();
     // Board_initWiFi();
 
-    /* Construct heartBeat Task  thread */
-    Task_Params_init(&taskParams);
-    taskParams.arg0 = 1000;
-    taskParams.stackSize = TASKSTACKSIZE;
-    taskParams.stack = &task0Stack;
-    Task_construct(&task0Struct, (Task_FuncPtr)heartBeatFxn, &taskParams, NULL);
-
     /* Turn on user LED */
     GPIO_write(Board_LED0, Board_LED_ON);
-
-    System_printf("Starting the example\nSystem provider is set to SysMin. "
-                  "Halt the target to view any SysMin contents in ROV.\n");
-    /* SysMin will only print to the console when you call flush or exit */
-    System_flush();
 
     /* Start BIOS */
     BIOS_start();
